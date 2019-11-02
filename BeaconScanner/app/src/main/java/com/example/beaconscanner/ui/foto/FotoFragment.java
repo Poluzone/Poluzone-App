d3eae16c74838a3f30864e8ea5a1845fa1d6d12c
IviRome <ivan.romero.ruiz@hotmail.com>
2019-11-02 15:17:16 +0100

    Guarda los permisos de almacenamiento y camara
    Pero falta concatenarlos adecuadamente y que se muestre la foto obetenida en el ImageView

--- BeaconScanner/app/src/main/java/com/example/beaconscanner/ui/foto/FotoFragment.java
+++ BeaconScanner/app/src/main/java/com/example/beaconscanner/ui/foto/FotoFragment.java
@@ -1,13 +1,23 @@
 package com.example.beaconscanner.ui.foto;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -19,19 +29,88 @@ public class FotoFragment extends Fragment {
     //Clase relacionada con el botón Polufoto
     //---------------------------------------------------------------------------
     private FotoViewModel fotoViewModel;
+    private final int PEDIR_PERMISO_CAMARA=1;
+    private ImageButton btnCamera;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         fotoViewModel =
                 ViewModelProviders.of(this).get(FotoViewModel.class);
         View root = inflater.inflate(R.layout.fragment_foto, container, false);
-        /*final TextView textView = root.findViewById(R.id.text_foto);
-        fotoViewModel.getText().observe(this, new Observer<String>() {
+        btnCamera= root.findViewById(R.id.foto_button);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View view) {
+
+                if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {// Marshmallow+
+                    if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                        // Should we show an explanation?
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                            // Show an expanation to the user *asynchronously* -- don't block
+                            // this thread waiting for the user's response! After the user
+                            // sees the explanation, try again to request the permission.
+                            // No se necesita dar una explicación al usuario, sólo pedimos el permiso.
+                            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                                    225);
+                            // MY_PERMISSIONS_REQUEST_CAMARA es una constante definida en la app. El método callback obtiene el resultado de la petición.
+                        }
+                    } /*else { //have permissions
+
+                    }*/
+                    if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        // Should we show an explanation?
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.CAMERA)) {
+                            // Show an expanation to the user *asynchronously* -- don't block
+                            // this thread waiting for the user's response! After the user
+                            // sees the explanation, try again to request the permission.
+                            // No se necesita dar una explicación al usuario, sólo pedimos el permiso.
+                            requestPermissions(new String[]{Manifest.permission.CAMERA},
+                                    PEDIR_PERMISO_CAMARA);
+                            // MY_PERMISSIONS_REQUEST_CAMARA es una constante definida en la app. El método callback obtiene el resultado de la petición.
+                        }
+                    } else { //have permissions
+                        Log.d("hola", "3");
+                        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        startActivityForResult(i, 0);
+                    }
+                } else { // Pre-Marshmallow
+                    Log.d("hola", "4");
+                    Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(i, 0);
+                }
             }
-        });*/ // Esto era para cambiar un texto que tenia por defecto la navigation Activity que te decia en que fragmento estabas
+        });
+
         return root;
     }
+
+    /*@Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+
+    }*/
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        Log.d("hola","si");
+        switch (requestCode) {
+            case PEDIR_PERMISO_CAMARA : {
+                // Si la petición es cancelada, el array resultante estará vacío.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d("hola","4");
+                    // El permiso ha sido concedido.
+                    Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(i,0);
+                } else {
+                    Log.d("hola","5");
+                    // Permiso denegado, deshabilita la funcionalidad que depende de este permiso.
+                }
+                return;
+            }
+            // otros bloques de 'case' para controlar otros permisos de la aplicación
+        }
+    }
+
 }
