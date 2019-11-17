a863a30d666a7202a2e19be57c032360031bde70
Emilia Rosa van der Heide <emvader@epsg.upv.es>
2019-11-17 20:18:54 +0100

    fragment creado

---
+++ BeaconScanner/app/src/main/java/com/example/beaconscanner/ui/inicio/InicioConductorFragment.java
@@ -0,0 +1,37 @@
+package com.example.beaconscanner.ui.inicio;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.beaconscanner.R;
+
+public class InicioConductorFragment extends Fragment {
+
+    private InicioConductorViewModel mViewModel;
+
+    public static InicioConductorFragment newInstance() {
+        return new InicioConductorFragment();
+    }
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        mViewModel =
+                ViewModelProviders.of(this).get(InicioConductorViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_inicio_conductor, container, false);
+
+        // acceder speed dial para esconderlo
+    /*    speedDialView = getParentFragment().getActivity().findViewById(R.id.fab);
+        speedDialView.hide();*/
+
+        return root;
+    }
+
+}
---
+++ BeaconScanner/app/src/main/java/com/example/beaconscanner/ui/inicio/InicioConductorViewModel.java
@@ -0,0 +1,7 @@
+package com.example.beaconscanner.ui.inicio;
+
+import androidx.lifecycle.ViewModel;
+
+public class InicioConductorViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
