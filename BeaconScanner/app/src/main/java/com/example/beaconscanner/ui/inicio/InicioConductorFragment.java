2512ff0736bb60195b25456e16539fc6f87e9138
Emilia Rosa van der Heide <emvader@epsg.upv.es>
2019-11-19 19:28:30 +0100

    layout terminado

--- BeaconScanner/app/src/main/java/com/example/beaconscanner/ui/inicio/InicioConductorFragment.java
+++ BeaconScanner/app/src/main/java/com/example/beaconscanner/ui/inicio/InicioConductorFragment.java
@@ -11,9 +11,11 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.example.beaconscanner.Medida;
 import com.example.beaconscanner.R;
