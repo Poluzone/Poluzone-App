2cc82e350016b6211d0a35739b9d1030be0cec1d
Emilia Rosa van der Heide <emvader@epsg.upv.es>
2019-11-19 12:13:29 +0100

    grafica metida

--- BeaconScanner/app/src/main/java/com/example/beaconscanner/ui/inicio/InicioConductorFragment.java
+++ BeaconScanner/app/src/main/java/com/example/beaconscanner/ui/inicio/InicioConductorFragment.java
@@ -11,8 +11,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.beaconscanner.Medida;
 import com.example.beaconscanner.R;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.leinardi.android.speeddial.SpeedDialView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class InicioConductorFragment extends Fragment {
 
@@ -32,6 +40,39 @@ public class InicioConductorFragment extends Fragment {
         // acceder speed dial para esconderlo
         speedDialView = getParentFragment().getActivity().findViewById(R.id.fab);
         speedDialView.hide();
+
+
+        // ------------------------ CHART --------------------------------------------------
+        LineChart chart = root.findViewById(R.id.chart);
+
+        Medida[] dataObjects = new Medida[10];
+        Medida medida = new Medida();
+        medida.setMedida(9.8f);
+        medida.setTiempo(9891);
+
+        Medida medida1 = new Medida();
+        medida1.setMedida(9.4f);
+        medida1.setTiempo(9892);
+
+        Medida medida2 = new Medida();
+        medida2.setMedida(9.1f);
+        medida2.setTiempo(9893);
+
+        dataObjects[0] = medida;
+        dataObjects[1] = medida1;
+        dataObjects[2] = medida2;
+
+
+        List<Entry> entries = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            entries.add(new Entry(dataObjects[i].getTiempo(), dataObjects[i].getMedida()));
+        }
+
+        LineDataSet dataSet = new LineDataSet(entries, "Medidas"); // add entries to dataset
+
+        LineData lineData = new LineData(dataSet);
+        chart.setData(lineData);
+        chart.invalidate(); // refresh
 
         return root;
     }
