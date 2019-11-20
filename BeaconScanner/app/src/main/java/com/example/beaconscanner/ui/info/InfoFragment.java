a6e799a120add1f40468320e43d64f83ee9675b3
Emilia Rosa van der Heide <emvader@epsg.upv.es>
2019-11-20 08:45:59 +0100

    fragment info implementado

---
+++ BeaconScanner/app/src/main/java/com/example/beaconscanner/ui/info/InfoFragment.java
@@ -0,0 +1,38 @@
+package com.example.beaconscanner.ui.info;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.beaconscanner.R;
+
+public class InfoFragment extends Fragment {
+
+    private InfoViewModel mViewModel;
+
+    public static InfoFragment newInstance() {
+        return new InfoFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_info, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(InfoViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ BeaconScanner/app/src/main/java/com/example/beaconscanner/ui/info/InfoViewModel.java
@@ -0,0 +1,7 @@
+package com.example.beaconscanner.ui.info;
+
+import androidx.lifecycle.ViewModel;
+
+public class InfoViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
