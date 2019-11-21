e0a8a043769fdba4355108d6a7811accfd518729
Jocarfl <jcarreresjr@hotmail.com>
2019-11-21 13:22:10 +0100

    fixes

--- BeaconScanner/app/src/main/java/com/example/beaconscanner/ServidorFake.java
+++ BeaconScanner/app/src/main/java/com/example/beaconscanner/ServidorFake.java
@@ -371,6 +371,45 @@ public class ServidorFake {
         queue.add(jsonobj);
         return id;
     }
+    // ---------------------------------------------------------------------------
+    // idUsuario, idSensor -> vincularIDdeUsuarioConSensor() ->
+    // ---------------------------------------------------------------------------
+    public void vincularIDdeUsuarioConSensor(int idUsuario, int idSensor) {
+        Log.d("pruebas", "vincularIDdeUsuarioConSensor()");
+        String url = "http://"+IP+":"+puerto+"/insertarIdUsuarioConIdsensor";
+
+        JSONObject datos = new JSONObject();
+
+
+        // Anyadimos los datos al json
+        try {
+            datos.put("IdUsuario", idUsuario);
+            datos.put("IdSensor", idSensor);
+            Log.d("pruebas json", datos.toString());
+        } catch (JSONException e) {
+            Log.d("pruebas", e.toString());
+        }
+
+        // Hacemos la peticion
+        JsonObjectRequest jsonobj = new JsonObjectRequest(Request.Method.POST, url, datos,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("pruebas",response.toString());
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("pruebas",error.toString());
+                    }
+                }
+        );
+
+        // Add the request to the RequestQueue.
+        queue.add(jsonobj);
+    }
 
     /**
      * Esta función sirve para comparar caracteres, uno a uno de forma que te diga si son o no numericos.
